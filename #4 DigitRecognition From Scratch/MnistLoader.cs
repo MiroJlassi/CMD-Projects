a2dc8e9f764c91b9bf39762fe36a9b45c8a90fb5
Marwen <marwen.jlassi.2020@gmail.com>
2025-08-24 00:36:00 +0100

    dataset loader

---
+++ #4 DigitRecognition From Scratch/MnistLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+public static class MnistLoader
+{
+    public static List<MnistSample> LoadData(string filePath)
+    {
+        var data = new List<MnistSample>();
+
+        foreach (var line in File.ReadLines(filePath))
+        {
+            var values = line.Split(',').Select(int.Parse).ToArray();
+            int label = values[0];
+            float[] pixels = values.Skip(1).Select(v => v / 255f).ToArray();
+
+            data.Add(new MnistSample { Label = label, Pixels = pixels });
+        }
+
+        return data;
+    }
+    public static void ShowImageFromCsvRow(MnistSample row)
+    {
+        int label = row.Label;
+        var pixels = row.Pixels;
+        Console.WriteLine($"Label: {label}");
+        for (int i = 0; i < 28; i++)
+        {
+            for (int j = 0; j < 28; j++)
+            {
+                float pixel = pixels[i * 28 + j];
+                char c = pixel > 200/255 ? '#' : pixel > 100/255 ? '+' : pixel > 50/255 ? '.' : ' ';
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ #4 DigitRecognition From Scratch/MnistSample.cs
@@ -0,0 +1,5 @@
+﻿public class MnistSample
+{
+    public float[] Pixels { get; set; }   // 784 normalized values
+    public int Label { get; set; }        // digit 0–9
+}
